93a262d1dcc28b3d37ee1800063e120d4b0bdc93
laurencevachon1 <laurence.vachon.1@ulaval.ca>
2023-01-27 01:51:27 -0500

    change db

--- backend/infra/ProblemsRepository.py
+++ backend/infra/ProblemsRepository.py
@@ -1,12 +1,12 @@
+
 from domain.constants import MONGO_HOST
-from services.UserRepository import UserRepository
 from pymongo import MongoClient
 
 
 client = MongoClient(MONGO_HOST)
 database_name = "Problem"
 
-class ProblemsRepository(UserRepository):
+class ProblemsRepository:
     def __init__(self):
         database = client[database_name]
         self.credentials_db = database['credentials']
