d90749e4fdf243baf7f64327614cad5d48c11b37
Marc-Anthony Girard <marcanthony.girard.pro@gmail.com>
2023-01-26 23:43:10 -0500

    allo

--- backend/api/problemController.py
+++ backend/api/problemController.py
@@ -11,10 +11,44 @@ class ProblemsController(ApiResource):
 
     def post(self):
         data = request.get_data()
-        #id = ProblemsRepository.sign_up(data['email'], data['password'])
+
+        ProblemsRepository().add_problem(data['name'], data['description'], data['type'], data['userId'])
 
         response = make_response()
         response.status_code = 200
         return response
 
+
+class NotificationsController(ApiResource):
+    @staticmethod
+    def path():
+        return "/notifications"
+
+    def get(self):
+        response = make_response()
+        response.status_code = 200
+        data = {
+            "notifications": [
+                {
+                    "is_read": False,
+                    "notification": {
+                        "name": "New Feature Released",
+                        "description": "A new feature has been released on the platform. Check it out!",
+                        "type": "update",
+                        "author_name": "John Smith"
+                    }
+                },
+                {
+                    "is_read": True,
+                    "notification": {
+                        "name": "Maintenance Scheduled",
+                        "description": "The platform will be undergoing maintenance on Wednesday at 2am. Please plan accordingly.",
+                        "type": "maintenance",
+                        "author_name": "Jane Doe"
+                    }
+                }
+            ]
+        }
+        response.data = data
+        return response
 
