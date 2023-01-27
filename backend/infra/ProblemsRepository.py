b73d4ae414f4fffbb0a9b10dbf3786d7cf271c6a
Marc-Anthony Girard <marcanthony.girard.pro@gmail.com>
2023-01-27 00:58:04 -0500

    small change

--- backend/infra/ProblemsRepository.py
+++ backend/infra/ProblemsRepository.py
@@ -3,10 +3,10 @@ from services.UserRepository import UserRepository
 from pymongo import MongoClient
 
 client = MongoClient("mongodb+srv://h21:mPythonMongo@cluster0.f9ba0.mongodb.net/?retryWrites=true&w=majority")
-
+database_name = "Problem"
 
 class ProblemsRepository(UserRepository):
-    def __init__(self, database_name, salt):
+    def __init__(self):
         database = client[database_name]
         self.credentials_db = database['credentials']
         self.tokens_db = database['tokens']
